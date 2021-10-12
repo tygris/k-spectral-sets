d9d603e441ef0d62a4bca2bf15bef7828307d5d3
tygris <draegon19@gmail.com>
2021-10-11 19:26:08 -0700

    test branching

--- define_del_Omega.m
+++ define_del_Omega.m
@@ -3,12 +3,16 @@
 %input, del_Omega_0, complex vector, the original boundary we are removing disks from.
 %       the first element of the vector needs to be the abscissa (angle
 %       zero from the center)
+%input, del_omega_0, integer vector, 0 indicates the point is originally
+%       from the matrix numerical range, non-zero integers indicate which
+%       disk the arclength refers to in order of removal
 %input, A, square matrix being analyzed
 %input, om, complex vector, the center of the circles to be removed
 %input, res, integer, the number of points on the boundary of the circle
 %input, radius, double vector, optional argument for the radius of the circles to
 %       be removed. Must be the same length and in the same order as om
 %output, del_Omega, complex vector of the closed boundary of the spectral set
+%output, del_omega, integer vector of update to del_omega_0
 %output, intersections, complex matrix of the points part of del_Omega closest
 %       to the intersection disk jj and the rest of del_Omega
 %       -the first row is the intersection counter-clockwise closest to the
@@ -43,14 +47,18 @@ function [del_Omega, del_omega, intersections, r_over_pi] = define_del_Omega(del
     ii = find(real(del_Omega_0)==max(real(del_Omega_0)), 1, 'first'); %location of the abscissa
     if (imag(del_Omega_0(ii)) - imag(del_Omega_0(ii+1))) > 0
         del_Omega = flip(del_Omega_0);
+        del_omega = flip(del_omega_0);
     else
         del_Omega = del_Omega_0;
+        del_omega = del_omega_0;
     end
     % define a for loop to remove all of the input circles.
     num_remove = length(om);
     r_over_pi = -1*ones(1,num_remove);
     intersections = [];
+    count_removed = max(del_omega); %the number of disks already removed
     for jj = 1:num_remove
+        count_removed = count_removed +1; %the number of the disk being removed
         %If the radius is not given as an input, call remove_circ without it
         if ~exist('radius', 'var')
             [del_Omega_jj, r_over_pi(jj)] = remove_circ(A, om(jj), res);
@@ -62,6 +70,8 @@ function [del_Omega, del_omega, intersections, r_over_pi] = define_del_Omega(del
         if (imag(del_Omega_jj(ij)) - imag(del_Omega_jj(ij+1))) > 0
             del_Omega_jj = flip(del_Omega_jj);
         end
+        %define the new vector to update del_omega with
+        del_omega_jj = count_removed*ones(1,length(del_Omega_jj));
         %Find Gamma 1
         Gam_jj = inpolygon(real(del_Omega_jj), imag(del_Omega_jj),real(del_Omega), imag(del_Omega));
         %find Gamma 0
@@ -75,6 +85,7 @@ function [del_Omega, del_omega, intersections, r_over_pi] = define_del_Omega(del
         if min(Gam_jj) > 0
             intersections = cat(2, intersections, [NaN]);
             del_Omega = [del_Omega, NaN+1i*NaN, del_Omega_jj]; %inner disks removed need to be in the clockwise direction
+            del_omega = [del_omega, NaN+1i*NaN, del_omega_jj];
         else
             %since we know it is not an annulus we need to find the points
             % of intersection. Using where Gam_0 first becomes a 1 as the estimate ensures
@@ -110,10 +121,13 @@ function [del_Omega, del_omega, intersections, r_over_pi] = define_del_Omega(del
                 negatives = imag(del_Omega_jj) >= 0 & Gam_jj ==1;
                 start = find(del_Omega_jj == min(del_Omega_jj(negatives)));
                 del_Omega = [del_Omega_jj(start:last_jj),del_Omega(~Gam_0), del_Omega_jj(first_jj:start)];
+                del_omega = [del_omega_jj(start:last_jj),del_omega(~Gam_0), del_omega_jj(first_jj:start)];
             elseif last_jj < first_jj
                 del_Omega = [del_Omega(1:first_0-1), del_Omega_jj(first_jj:end), del_Omega_jj(1:last_jj), del_Omega(last_0+1:end)];
+                del_omega = [del_omega(1:first_0-1), del_omega_jj(first_jj:end), del_omega_jj(1:last_jj), del_omega(last_0+1:end)];
             else
                 del_Omega = [del_Omega(1:first_0-1), del_Omega_jj(Gam_jj), del_Omega(last_0+1:end)];
+                del_omega = [del_omega(1:first_0-1), del_omega_jj(Gam_jj), del_omega(last_0+1:end)];
             end
         end
     end
