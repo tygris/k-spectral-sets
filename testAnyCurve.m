0eeefd39e0c2a96df292ddfa6a84722972d88df7
tygris <draegon19@gmail.com>
2021-11-01 16:30:16 -0700

    Added the Panamanian Rainforest example.

--- testAnyCurve.m
+++ testAnyCurve.m
@@ -27,9 +27,9 @@ figure(), plot(del_OmA1), daspect([1,1,1])
 [c2A1, minr_A1, A1_1or2] = calc_c2(A1, del_OmA1, del_OmA1_prime, 1, 1000)
  %for this example the minimum value of c2 happens on the real line (del_OmA1(1))
  
- %% Calculate c1 for A1 and this reshaped boundary
- c1A1 = c1_estimate(1, -pi/2, del_OmA1)
- c1A1 = c1_estimate(1, 3*pi/2, del_OmA1)
+ % Calculate c1 for A1 and this reshaped boundary
+ c1A1 = calc_c1(1, -pi/2, del_OmA1)
+ c1A1 = calc_c1(1, 3*pi/2, del_OmA1)
  %UHOH!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
  
  %first error is symmetric across the break point gets a zero:
@@ -43,7 +43,7 @@ figure(), plot(del_OmA1), daspect([1,1,1])
  angle_0 = mod(angle(del_OmA1(2)-del_OmA1(1)), 2*pi);
  angle_0 = min(abs(angle_0 - mod(-pi/2+pi, 2*pi)), abs(-pi/2 -angle_0));
  
-%% calculate k for A1 and this reshaped boundary 
+% calculate k for A1 and this reshaped boundary 
  
 k = calc_k(A1, del_OmA1, del_OmA1_prime, 1, 1, 1000)
 
@@ -53,11 +53,38 @@ k = calc_k(A1, del_OmA1, del_OmA1_prime, 1, 1, 1000)
 
  %% Find c2 for the complicated Panamanian Rainforest example from Neubert and Caswell
  
+A2 = [-1.5622, 0.6685,0,0,0,0,0,0,0;
+     0, -0.7119,0,0,2.5632,0,0,0,0;
+     1.4627, 0.0364, -6.4091,0,0,1.1446,0,55.8201,17.2972;
+     0,0,0,-0.0222,0,0,315.9443,0,0;
+     0,0,0,0.0201,-2.5632,0,0,0,0;
+     0,0.0070,0,0,0,-2.0348,0,0,0;
+     0,0,6.4091,0,0,0,-315.9443,0,0;
+     0.0995,0,0,0,0,0.8902,0,-62.6458,0;
+     0,0,0,0,0,0,0,6.8257,-17.2972];
  
- 
- 
- 
- 
+A2_fov = fov(A2);
+A2_fov_prime = diff(A2_fov);
+
+% convert to discrete vectors with nump points
+L = 2*pi;
+nump = 1000;
+ds = L/(nump-1);
+del_OmA2 = A2_fov([0:nump]*ds);
+del_OmA2_prime = A2_fov_prime([0:nump]*ds);
+
+% reshape del_Omega so that the real part is always less than zero
+delta = 1e-6;
+vertical_index = (real(del_OmA2)> -delta);
+del_OmA2(vertical_index) = -delta*ones(1,sum(vertical_index)) + 1i*imag(del_OmA2(vertical_index));
+del_OmA2_prime(vertical_index) = -1i;
+figure(), plot(del_OmA2), daspect([1,1,1])
+plot(A2_eigs, zeros(1, 9),'x')
+
+% Calculate c1 and the min eigenvalues for A2
+
+c1A2 = calc_c1(1, -pi/2, del_OmA2)
+[c2A2, minr_A2, A2_1or2] = calc_c2(A2, del_OmA2, del_OmA2_prime, .1, 1000)
  
  
  
