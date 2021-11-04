1cc3fb8f45193faaa0952b31062a700ef680feb2
tygris <draegon19@gmail.com>
2021-11-04 12:40:34 -0700

    Updated numerical_range to use chebfun and optionally return the derivative at each point. calc_c2 actually calculates c2, by calculating the min eigenvalue (as before), and the arclength of the curve.

--- calc_c2.m
+++ calc_c2.m
@@ -27,25 +27,66 @@ This function assumes that for overlapping disks, c2 is calculated by
 
 %Currently the negative of the minimum eigenvalue is being calculated instead
 % of c2
-function [c2, min_r, r1orr2] = calc_c2(A, del_Om, del_Om_prime, max_length, resolution)
-    %Check that the inputs are reasonable
+function [c2, mineig, L] = calc_c2(A, nr, nr_prime, del_Om, del_Om_prime, max_length, resolution)
+    %Check that the fun inputs satisfy the requirements
+    assert(nargin == 7, ...
+        "All function inputs are required: A, nr, nr_prime, del_Om, del_Om_prime, max_length, resolution.")
     [n,m] = size(A);
     assert(n==m, "A must be a square matrix")
     assert(length(del_Om) == length(del_Om_prime), ...
-        "The derivative is needed for every point on the boundary.")    
+        "The derivative is needed for every point on the boundary.") 
+    
+    %define Gamma 1 and the derivative of Gamma 1
+    nr = nr(1:end-1); del_Om = del_Om(1:end-1); %ignore repeated value
+    in1 = ~ismember(nr, del_Om);
+    in2 = ~ismember(del_Om, nr);
+    % if the spectral set is equal to the numerical range, then the
+    % operator is already positive definite
+    if sum(in2) == 0
+        c2 = 2;
+        mineig = 0; L = NaN;
+        return
+    end
+    %otherwise we continue creating Gamma 1
+    if in1(1)
+        ind1 = in1 & imag(nr)>=0;
+        ind2 = in2 & imag(del_Om)>=0;
+        ind3 = in1 & imag(nr) < 0;
+        ind4 = in2 & imag(del_Om) < 0;
+        gam1 = cat(2, nr(ind1), flip(del_Om(ind2)), flip(del_Om(ind4)), nr(ind3), nr(1));
+        gam1_prime = cat(2, nr_prime(ind1), flip(del_Om_prime(ind2)), flip(del_Om_prime(ind4)), nr_prime(ind3), nr_prime(1));
+        figure(), plot(gam1), hold on
+    else
+        temp = nr(in1);
+        tempp = nr_prime(in1);
+        gam1 = cat(2, temp, flip(del_Om(in2)), temp(1));
+        gam1_prime = cat(2, tempp, flip(del_Om_prime(in2)), tempp(1));
+        figure(), plot(gam1), hold on
+    end
+    %calculate the arclength of gam1 using the absolute distance between
+    %points
+    dz = abs(gam1(2:end) - gam1(1:end-1));
+    L = sum(dz);
+    
+    %notice that we only need to calculate values of r along the
+    %     intersection of gam1 and nr
+    search_points = del_Om(in2);
+    search_primes = del_Om_prime(in2);
     %calculate an initial estimate for the worst-case r 
     % can be done at any point along del_Om, so choose the first
-    [min_r, r1orr2] = findr(A, del_Om(1), del_Om_prime(1), max_length, resolution);
+    [min_r, r1orr2] = findr(A, search_points(1), search_primes(1), max_length, resolution);
     % find next point along del_om where the maximum allowed radius is
     % smaller than min_r
-    for ii = 2:length(del_Om)
-        new_om = del_Om(ii)+min_r*1i*del_Om_prime(ii);
+    for ii = 2:length(search_points)
+        new_om = search_points(ii)+min_r*1i*search_primes(ii);
         [new_r1, new_r2] = r_of_A(A, m, new_om);
         new_r = max(new_r1, new_r2);
         if new_r<min_r
             %find the new minimum value of r along del_Omega
-            [min_r, r1orr2] = findr(A, del_Om(ii), del_Om_prime(ii), min_r, resolution);            
+            [min_r, r1orr2] = findr(A, search_points(ii), search_primes(ii), min_r, resolution);            
         end
     end
-    c2 = (r1orr2)/(2*pi*min_r);
+    mineig = (r1orr2)/(2*pi*min_r);
+    S1 = mineig*L;
+    c2 = 2 + 2*S1;
 end
--- testAnyCurve.m
+++ testAnyCurve.m
@@ -12,19 +12,19 @@ sigma0_prime = -1i;
 [rA1, A1_1or2] = findr(A1, sigma0, sigma0_prime, 1, 1000)
 
 %compare to previously tested plots
-A1num_range = numerical_range(A1, 100);
-spectral_set_choices(A1, A1num_range, 50);
+[nrA1, nrA1_prime] = numerical_range(A1, 100);
+%spectral_set_choices(A1, nrA1, 50);
 
 %% Test calc_c2 with A1
 % define del_Om for testing
-del_OmA1 = A1num_range;
+del_OmA1 = nrA1;
 del_OmA1_prime = -1i*(del_OmA1 +1/2);
 vertical_index = (real(del_OmA1)> -1/4);
 del_OmA1(vertical_index) = -1/4*ones(1,sum(vertical_index)) + 1i*imag(del_OmA1(vertical_index));
 del_OmA1_prime(vertical_index) = -1i;
 figure(), plot(del_OmA1), daspect([1,1,1])
 
-[c2A1, minr_A1, A1_1or2] = calc_c2(A1, del_OmA1, del_OmA1_prime, 1, 1000)
+[c2A1, minr_A1, A1_1or2] = calc_c2(A1, nrA1, nrA1_prime, del_OmA1, del_OmA1_prime, 1, 1000)
  %for this example the minimum value of c2 happens on the real line (del_OmA1(1))
  
  % Calculate c1 for A1 and this reshaped boundary
@@ -45,11 +45,11 @@ figure(), plot(del_OmA1), daspect([1,1,1])
  
 % calculate k for A1 and this reshaped boundary 
  
-k = calc_k(A1, del_OmA1, del_OmA1_prime, 1, 1, 1000)
+%k = calc_k(A1, del_OmA1, del_OmA1_prime, 1, 1, 1000)
 
 %% What happens when the numerical range is passed for calculating c2 (nothing removed)?
 
-[c2A1_2, minr_A1_2, A1_1or2_2] = calc_c2(A1, A1num_range, -1i*(del_OmA1 +1/2), 1, 1000)
+[c2A1_2, minr_A1_2, A1_1or2_2] = calc_c2(A1, nrA1, nrA1_prime, nrA1, -1i*(del_OmA1 +1/2), 1, 1000)
 
  %% Find c2 for the complicated Panamanian Rainforest example from Neubert and Caswell
  
@@ -62,36 +62,78 @@ A2 = [-1.5622, 0.6685,0,0,0,0,0,0,0;
      0,0,6.4091,0,0,0,-315.9443,0,0;
      0.0995,0,0,0,0,0.8902,0,-62.6458,0;
      0,0,0,0,0,0,0,6.8257,-17.2972];
- 
+A2_eigs = eig(A2);
+
 A2_fov = fov(A2);
 A2_fov_prime = diff(A2_fov);
 
 % convert to discrete vectors with nump points
 L = 2*pi;
 nump = 1000;
-ds = L/(nump-1);
-del_OmA2 = A2_fov([0:nump]*ds);
-del_OmA2_prime = A2_fov_prime([0:nump]*ds);
+ds = L/(nump);
+nrA2 = A2_fov([0:nump]*ds);
+nrA2_prime = A2_fov_prime([0:nump]*ds);
+nrA2 = flip(nrA2);
+nrA2_prime = flip(nrA2_prime);
 
 % reshape del_Omega so that the real part is always less than zero
 delta = 1e-6;
+del_OmA2 = nrA2; del_OmA2_prime = nrA2_prime;
 vertical_index = (real(del_OmA2)> -delta);
 del_OmA2(vertical_index) = -delta*ones(1,sum(vertical_index)) + 1i*imag(del_OmA2(vertical_index));
 del_OmA2_prime(vertical_index) = -1i;
 figure(), plot(del_OmA2), daspect([1,1,1])
+hold on
 plot(A2_eigs, zeros(1, 9),'x')
 
 % Calculate c1 and the min eigenvalues for A2
 
 c1A2 = calc_c1(1, -pi/2, del_OmA2)
-[c2A2, minr_A2, A2_1or2] = calc_c2(A2, del_OmA2, del_OmA2_prime, .1, 1000)
+[c2A2, minr_A2, A2_1or2] = calc_c2(A2, nrA2, nrA2_prime, del_OmA2, del_OmA2_prime, .1, 1000)
  
  
- 
- 
- 
- 
- 
+%% Test curve creation for measuring arclength in calc_c2
+
+A1 = [.5 -1; 1 -1.5];
+nrA1 = numerical_range(A1, 100);
+del_OmA1 = nrA1;
+del_OmA1_prime = -1i*(del_OmA1 +1/2);
+vertical_index = (real(del_OmA1)> -1/4);
+del_OmA1(vertical_index) = -1/4*ones(1,sum(vertical_index)) + 1i*imag(del_OmA1(vertical_index));
+del_OmA1_prime(vertical_index) = -1i;
+fova = nrA1;
+del_Om = del_OmA1;
+in1 = ~ismember(fova, del_Om);
+in2 = ~ismember(del_Om, fova);
+out = fova(in1);
+in = fova(~in1);
+figure(), plot(out), daspect([1,1,1]), hold on
+plot(out(1:4), 'o')
+plot(in(1), 'o')
+
+%%
+in1 = ~ismember(nr, del_Om);
+in2 = ~ismember(del_Om, nr);
+if in1(1)
+    ind1 = in1 & imag(nr)>=0;
+    ind2 = in2 & imag(del_Om)>=0;
+    ind3 = in1 & imag(nr) < 0;
+    ind4 = in2 & imag(del_Om) < 0;
+    gam1 = cat(2, nr(ind1), flip(del_Om(ind2)), flip(del_Om(ind4)), nr(ind3), nr(1));
+%    gam1_prime = cat(2, nr_prime(ind1), flip(del_Om_prime(ind2)), flip(del_Om_prime(ind4)), nr_prime(ind3), nr_prime(1));
+    figure(), plot(gam1), hold on
+else
+    temp = nr(in1);
+    tempp = nr_prime(in1);
+    gam1 = cat(2, temp, flip(del_Om(in2)), temp(1));
+%    gam1_prime = cat(2, tempp, flip(del_Om_prime(in2)), tempp(1));
+    figure(), plot(gam1), hold on
+end
+%calculate the arclength of gam1 using the absolute distance between
+%points
+dz = abs(gam1(2:end) - gam1(1:end-1));
+L = sum(dz)
+
  
  
  
