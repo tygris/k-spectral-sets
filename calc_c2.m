31bba22751dab9932da32243fed6e754a23ff75d
tygris <draegon19@gmail.com>
2022-01-02 15:50:08 -0800

    Sped up calc_c2, and included the general upper bound M for f on the numerical range.

--- calc_c2.m
+++ calc_c2.m
@@ -4,7 +4,7 @@
 % This function assumes that for overlapping disks, c2 is calculated by the
 % methodology in ref [1]
 % 
-% [c2, mineig, L] = calc_c2(A, nr, nr_prime, del_Om, del_Om_prime, max_length, resolution)
+% [c2, mineig, L] = calc_c2(A, nr, nr_prime, del_Om, del_Om_prime, max_length, resolution, M)
 %  input, A, square matrix A that is an input to some function f
 %  input, nr, complex vector, the boundary of the numerical range of A
 %  input, nr_prime, complex vector, the corresponding derivatives of the
@@ -17,6 +17,8 @@
 %         removed disk to be. 1 is the default.
 %  input, resolution, integer, the number discretization points to use while
 %         searching for the maximum distance om can be from del_Om
+%  input (opt), M, double, ||f||_{\Omega} for the function we are bounding 
+%         with the spectral set
 %  output, c2, double, the value of c2 defining the k-spectral-set
 %  output, mineig, double, the minimum value of an eigenvalue of mu(A,f) in
 %          the region of the numerical range of A that is not part of Omega
@@ -31,12 +33,15 @@
 % [1] Wellen and Greenbaum, K-Spectral Sets
 
 %Natalie Wellen
-%12/31/21
+%1/02/21
 
-function [c2, mineig, L] = calc_c2(A, nr, nr_prime, del_Om, del_Om_prime, max_length, resolution)
+function [c2, mineig, L] = calc_c2(A, nr, nr_prime, del_Om, del_Om_prime, max_length, resolution, M)
     %Check that the fun inputs satisfy the requirements
-    assert(nargin == 7, ...
-        "All function inputs are required: A, nr, nr_prime, del_Om, del_Om_prime, max_length, resolution.")
+    assert(nargin >= 7, ...
+        "First seven function inputs are required: A, nr, nr_prime, del_Om, del_Om_prime, max_length, resolution.")
+    if nargin == 7
+        M = 1;
+    end
     [n,m] = size(A);
     assert(n==m, "A must be a square matrix")
     assert(length(del_Om) == length(del_Om_prime), ...
@@ -90,12 +95,19 @@ function [c2, mineig, L] = calc_c2(A, nr, nr_prime, del_Om, del_Om_prime, max_le
     
     %notice that we only need to calculate values of r along gam1
     %calculate an initial estimate for the worst-case r 
-    
+    eigenvs = eig(A);
+    m = length(eigenvs);
+    B = zeros(length(gam1), m); %holds distances
+    for ii = 1:m
+        B(:, ii) = abs(gam1.'-eigenvs(ii));
+    end
+    [val, rows] = min(B);
+    [val, col] = min(val);
     % change to be the point closest to an eigenvalue
-    [min_r, r1orr2] = findr(A, gam1(1), gam1_prime(1), max_length, resolution);
+    [min_r, r1orr2] = findr(A, gam1(rows(col)), gam1_prime(rows(col)), max_length, resolution);
     % find next point along del_om where the maximum allowed radius is
     % smaller than min_r
-    for ii = 2:length(gam1)
+    for ii = 1:length(gam1)
         new_om = gam1(ii)+min_r*1i*gam1_prime(ii);
         [new_r1, new_r2] = r_of_A(A, m, new_om);
         new_r = max(new_r1, new_r2);
@@ -105,6 +117,6 @@ function [c2, mineig, L] = calc_c2(A, nr, nr_prime, del_Om, del_Om_prime, max_le
         end
     end
     mineig = (r1orr2)/(2*pi*min_r);
-    S1 = mineig*(L+G); %equivalent to ||S_1|| + ||S_2|| in the paper 
-    c2 = (2 + S1)/2; %||S(f,A)|| <= 2c_2; 
+    S1 = mineig*(M*L+G); %equivalent to ||S_1|| + ||S_2|| in the paper 
+    c2 = (2*M + S1)/2; %||S(f,A)|| <= 2c_2; 
 end
