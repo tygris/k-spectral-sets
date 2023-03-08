972b150a29ae2c69c2130a2882bb0d48306ca7d7
tygris <draegon19@gmail.com>
2023-03-07 21:06:49 -0800

    Remove files that were renamed.

--- calc_k_removed_disks.m
+++ calc_k_removed_disks.m
@@ -1,11 +1,11 @@
 %Function to calculate the K value and integral of the resolvent norm for a
 % set Omega that is defined by removing disks from the numerical range of
-% the input matrix A. This function is best used in conjunction with the 
-% outputs of define_del_Omega().
+% the input matrix A. This function is best used on the results of
+% define_del_Omega.
 %
-%[k, cif, delOmPrime, c1, c2] = calc_kRemovedDisk(A, om, nr, nr_prime, delOm, delom, xs, r1orr2)
+%[k, resNorm, delOm_prime, c1, c2] = calc_kRemovedDisk(A, zeta, nr, nr_prime, delOm, delom, xs, r1orr2)
 %  input, A, n by n double, Matrix of interest
-%  input, om, complex vector, the center of the disks removed from W(A) to
+%  input, zeta, complex vector, the center of the disks removed from W(A) to
 %         define Omega
 %  input, nr, complex vector, the boundary of the numerical range of A, W(A)
 %  input, nr_prime, complex vector, the derivative at each corresponding
@@ -13,7 +13,7 @@
 %  input, delOm, complex vector, the boundary of the spectral set Omega
 %         defined by removing disks from the numerical range of A
 %  input, delom, integer vector, 0 indicates the corresponding point in delOm 
-%         originates from W(A), other values are the index of om indicating 
+%         originates from W(A), other values are the index of zeta indicating 
 %         which disk's boundary the point was originally located on
 %  input, xs, complex vector, the intersection points of delOmega and the
 %         boundary of each consecutive disk removed
@@ -22,7 +22,7 @@
 %         conditions for r2 from Theorem 2, but not r1. 
 %
 %  output, k, double, the K value of the spectral set Omega
-%  output, cif, double, the value of the integral of the resolvent norm of A
+%  output, resNorm, double, the value of the integral of the resolvent norm of A
 %  output, delOm_prime, complex double, the derivative of delOm in the
 %          counter-clockwise direction
 %  output, c1, double
@@ -32,7 +32,7 @@
 %    - calc_c1
 %        - find_c1
 %    - calc_c2_curve
-%    - cauchyIntFormula
+%    - resolvent_norm_integral
 
 %Natalie Wellen
 %3/06/23
@@ -46,16 +46,15 @@
 %GOAL: estimate the derivative in the calculation rather than passing it to
 %the function, or have some kind of derivative check?
 
-function [k, cif, delOm_prime, c1, c2] = calc_kRemovedDisk(A, om, nr, nr_prime, delOm, delom, xs, r1orr2)
-    %calculate K and the integral of the resolvent norm for A
+function [k, resNorm, delOm_prime, c1, c2] = calc_k_removed_disks(A, zeta, nr, nr_prime, delOm, delom, xs, r1orr2)
     %define delOmPrime
     indBreaks = find(ismember(nr,xs));
     delOm_prime = zeros(1, length(delOm));
         %first along points of delOm that coincide with the numerical range
     primer = [];
 
-    %This code is a temporary fix to allow for calc when nr is split by
-    %removing disks.
+    %This code is specifically to allow for calcuting K when nr is split by
+    % removing disk(s). Aka when there are no annuli
     if ~delom(1) %false implies 1:indBreaks(1) is where nr_prime goes
         breaks = [1, indBreaks(1), indBreaks(end), length(nr)];
         if length(indBreaks)>2
@@ -72,14 +71,14 @@ function [k, cif, delOm_prime, c1, c2] = calc_kRemovedDisk(A, om, nr, nr_prime, 
     delOm_prime(delom == 0) = primer;
         %then with points along boundaries of the removed disks
     for jj = 1:max(delom)
-        om_now = om(jj);
+        zetaNow = zeta(jj);
         kk = find(delom == jj);
-        radius = abs(delOm(kk(1)) - om_now);
-        delOm_prime(kk) = -1i*((delOm(kk) - om_now)/radius);
+        radius = abs(delOm(kk(1)) - zetaNow);
+        delOm_prime(kk) = -1i*((delOm(kk) - zetaNow)/radius);
     end
     % c1 is numerically estimated
     [c1] = calc_c1(delOm, delOm_prime);
-    %We use Thoerem 2 to calculate an upper bound on c2
+    %We use Thoerem 2 from "K-Spectral Sets" to calculate an upper bound on c2
     c2_1 = 1+sum(r1orr2);
     %Alternatively, use Theorem 1 to estimate the value of c2 
     % Recall that on the boundary of the numerical range the integral of c2=0
@@ -105,12 +104,15 @@ function [k, cif, delOm_prime, c1, c2] = calc_kRemovedDisk(A, om, nr, nr_prime, 
         end
         c2_est = c2_est+1;
     end
+    %Choose the calculation of c2 with the smallest value
     c2 = (c2_1 <= c2_est)*c2_1 + (c2_est<c2_1)*c2_est;
+    %Calculate K
     k = c2+sqrt(c1+c2^2);
     breaks = find(isnan(delOm));
     breaks = cat(2, breaks, length(delOm)+1);
-    cif = cauchyIntFormula(A, delOm(1:breaks(1)-1));
+    %Calculate the integral of the resolvent norm for each closed curve of delOm
+    resNorm = resolvent_norm_integral(A, delOm(1:breaks(1)-1));
     for jj = 2:length(breaks)
-        cif = cif + cauchyIntFormula(A, delOm(breaks(jj-1)+1:breaks(jj)-1));
+        resNorm = resNorm + resolvent_norm_integral(A, delOm(breaks(jj-1)+1:breaks(jj)-1));
     end
 end
